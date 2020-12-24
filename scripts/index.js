08fe21ff740409c9c402a5a38593a333e49fba39
Websane <websanego@gmail.com>
2020-12-24 12:06:34 +0300

    fixed scroll

--- scripts/index.js
+++ scripts/index.js
@@ -29,30 +29,29 @@ function removeActive() {
     })
 }
 
+function addActive(link, menu) {
+  link.classList.add('dropdowns__link-isActive');
+  menu.classList.add('dropdowns__isActive');
+}
+
 const canHover = window.matchMedia('(hover: hover)').matches;
 
-document.querySelectorAll('.dropdowns__title').forEach((el) => {
-    el.addEventListener('click', (ev) => {
-        ev.preventDefault();
-        const link = el.children[0];
-        const menu = el.children[1];
-        if (link.classList.contains('dropdowns__link-isActive')) {
-            removeActive();
-            return;
-        }
-        removeActive();
-        link.classList.add('dropdowns__link-isActive');
-        menu.classList.add('dropdowns__isActive');
-        }
-    )
-    if (canHover) {
-        el.addEventListener('mouseover', () => {
-            const link = el.children[0];
-            const menu = el.children[1];
-            link.classList.add('dropdowns__link-isActive');
-            menu.classList.add('dropdowns__isActive');
-        })
+document.querySelectorAll('.dropdowns__element').forEach((el) => {
+  const link = el.children[0];
+  const menu = el.children[1];
+  el.addEventListener('click', (ev) => {
+    ev.preventDefault();
+    if (link.classList.contains('dropdowns__link-isActive')) {
+      removeActive();
+      return;
     }
+    removeActive();
+    addActive(link, menu);
+    }
+  )
+  if (canHover) {
+    el.addEventListener('mouseover', () => { addActive(link, menu) });
+  }
 })
 
 document.addEventListener('mouseout', (ev) => {
@@ -73,61 +72,61 @@ document.querySelectorAll('.dropdowns__menu').forEach((el) => {
 });
 
 // //бургер
-const burger = document.querySelector('.burger__btn');
-const menu = document.querySelector('.burger__menu');
-const body = document.querySelector('body');
-const burgerLink = document.querySelectorAll('.burger__link');
+// const burger = document.querySelector('.burger__btn');
+// const menu = document.querySelector('.burger__menu');
+// const body = document.querySelector('body');
+// const burgerLink = document.querySelectorAll('.burger__link');
 
-const open = () => {
-    burgerLink.forEach(el => {
-        burger.classList.contains('burger__btn-active') ?
-            el.tabIndex = -1 : el.tabIndex = 0;
-    })
-    burger.classList.toggle('burger__btn-active');
-    menu.classList.toggle('burger__menu-on');
-    body.classList.toggle('overflow');
-}
+// const open = () => {
+//     burgerLink.forEach(el => {
+//         burger.classList.contains('burger__btn-active') ?
+//             el.tabIndex = -1 : el.tabIndex = 0;
+//     })
+//     burger.classList.toggle('burger__btn-active');
+//     menu.classList.toggle('burger__menu-on');
+//     body.classList.toggle('overflow');
+// }
 
-const close = () => {
-    burgerLink.forEach(el => el.tabIndex = -1);
-    burger.classList.remove('burger__btn-active');
-    menu.classList.remove('burger__menu-on');
-    body.classList.remove('overflow');
-}
+// const close = () => {
+//     burgerLink.forEach(el => el.tabIndex = -1);
+//     burger.classList.remove('burger__btn-active');
+//     menu.classList.remove('burger__menu-on');
+//     body.classList.remove('overflow');
+// }
 
-burger.addEventListener('click', () => open());
-burgerLink.forEach(el => el.addEventListener('click', () => close()));
+// burger.addEventListener('click', () => open());
+// burgerLink.forEach(el => el.addEventListener('click', () => close()));
 
 //показ полосы поиска
-const search = document.querySelector('.search__top');
-const buttonSearch = document.querySelector('.header__button');
-const labelSearch = document.querySelector('.header__label');
-const burgerBtn = document.querySelector('.burger__btn');
-const logo = document.querySelector('.header__logo');
-const closeButton = document.createElement('button');
+// const search = document.querySelector('.search__top');
+// const buttonSearch = document.querySelector('.header__button');
+// const labelSearch = document.querySelector('.header__label');
+// const burgerBtn = document.querySelector('.burger__btn');
+// const logo = document.querySelector('.header__logo');
+// const closeButton = document.createElement('button');
 
-buttonSearch.addEventListener('click', (ev) => {
-    ev.preventDefault();
-    if (document.documentElement.clientWidth < 1367) {
-        labelSearch.classList.toggle('block');
-    }
-    if (document.documentElement.clientWidth < 769) {
-        search.append(closeButton);
-        closeButton.classList.toggle('search__top_close');
-        [burgerBtn, logo].forEach(el => el.classList.toggle('block-none'));
-        [labelSearch, search].forEach(el => el.classList.toggle('width-full'));
-    }
-    if (document.documentElement.clientWidth < 591) {
-        search.classList.toggle('padding-search');
-    }
-})
-closeButton.addEventListener('click', () => {
-    [labelSearch, search].forEach(el => el.classList.remove('width-full'));
-    labelSearch.classList.remove('block');
-    closeButton.classList.remove('search__top_close');
-    search.classList.remove('padding-search');
-    [burgerBtn, logo].forEach(el => el.classList.remove('block-none'));
-})
+// buttonSearch.addEventListener('click', (ev) => {
+//     ev.preventDefault();
+//     if (document.documentElement.clientWidth < 1367) {
+//         labelSearch.classList.toggle('block');
+//     }
+//     if (document.documentElement.clientWidth < 769) {
+//         search.append(closeButton);
+//         closeButton.classList.toggle('search__top_close');
+//         [burgerBtn, logo].forEach(el => el.classList.toggle('block-none'));
+//         [labelSearch, search].forEach(el => el.classList.toggle('width-full'));
+//     }
+//     if (document.documentElement.clientWidth < 591) {
+//         search.classList.toggle('padding-search');
+//     }
+// })
+// closeButton.addEventListener('click', () => {
+//     [labelSearch, search].forEach(el => el.classList.remove('width-full'));
+//     labelSearch.classList.remove('block');
+//     closeButton.classList.remove('search__top_close');
+//     search.classList.remove('padding-search');
+//     [burgerBtn, logo].forEach(el => el.classList.remove('block-none'));
+// })
 
 // //плавная прокрутка
 // const anchors = document.querySelectorAll('a[href*="#"]')
